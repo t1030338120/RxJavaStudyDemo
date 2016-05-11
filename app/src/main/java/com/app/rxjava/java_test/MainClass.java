218f7ab63bf443fe017864dfa433944feb1ae90e
t1030338120 <1030338120@qq.com>
2016-05-11 13:50:48 +0800

    测试git提交功能

---
+++ app/src/main/java/com/app/rxjava/java_test/MainClass.java
@@ -0,0 +1,14 @@
+package com.app.rxjava.java_test;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 描述：
+ * 作者：tyc
+ */
+public class MainClass {
+
+    View mView;
+    ViewGroup mViewGroup;
+}
