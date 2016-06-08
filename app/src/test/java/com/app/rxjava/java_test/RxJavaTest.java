8dbed949c54db698dbdfca7649815562c1745f4f
t1030338120 <1030338120@qq.com>
2016-06-08 14:28:02 +0800

    Rxjava retry and retryWhen 测试

--- app/src/test/java/com/app/rxjava/java_test/RxJavaTest.java
+++ app/src/test/java/com/app/rxjava/java_test/RxJavaTest.java
@@ -1,75 +1,33 @@
 package com.app.rxjava.java_test;
 
-import com.app.rxjava.dagger2.Container;
-import com.app.rxjava.dagger2.DaggerFruitComponent;
-
 import org.junit.Test;
 
 import rx.Observable;
-import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Func2;
 
 /**
- * 描述：
+ * 描述： RxJava操作符练习
  * 作者：tyc
  */
-public class MainClassTest {
+public class RxJavaTest {
+
+    String data = null;
+
 
     @Test
     public void testInsertSort() throws Exception {
         System.out.println("=================");
     }
 
-    @Test
-    public void testDagger2() throws Exception {
 
-        Container container = new Container();
-        DaggerFruitComponent.create().inject(container);
-        System.out.println(container.mApple.getName());
-        System.out.println(container.mBanana.getName());
-
-
-    }
-
-
-    @Test
-    public void testRxJava() {
-
-        Observable.just("123")
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        System.out.println("====" + s);
-                    }
-                });
-    }
-
-
-    @Test
-    public void testRxJava02() {
-        Observable<String> observable = Observable.just("123")
-                .doOnNext(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        System.out.println("doOnNext " + s);
-                    }
-                });
-
-        observable.subscribe(s -> System.out.println(s));
-
-
-    }
-
-
-    String data = null;
 
     @Test
     public void testRetryWhen() {
-//        retryWhen和retry类似，区别是，retryWhen将onError中的Throwable传递给一个函数，
-//        这个函数产生另一个Observable，retryWhen观察它的结果再决定是不是要重新订阅原始的Observable。
-//        如果这个是发射数据的Observable（正常Observable），它就重新订阅；如果这个是onError通知类型Observable，它就将这个通知传递给观察者然后终止。
+        //        retryWhen和retry类似，区别是，retryWhen将onError中的Throwable传递给一个函数，
+        //        这个函数产生另一个Observable，retryWhen观察它的结果再决定是不是要重新订阅原始的Observable。
+        //        如果这个是发射数据的Observable（正常Observable），它就重新订阅；如果这个是onError通知类型Observable，它就将这个通知传递给观察者然后终止。
 
 
         Observable.just("aaaa")
@@ -114,7 +72,8 @@ public class MainClassTest {
 
     @Test
     public void testRetry() {
-
+        //  Retry操作符不会将原始Observable的onError通知传递给观察者，它会订阅这个Observable，再给它一次机会无错误地完成它的数据序列。
+        // Retry总是传递onNext通知给观察者，由于重新订阅，可能会造成数据项重复，如上图所示。
         Observable.just(data)
                 .map(new Func1<String, String>() {
                     @Override
@@ -142,39 +101,6 @@ public class MainClassTest {
                 })
                 .subscribe(s -> System.out.println("onNext " + s),
                         throwable -> System.out.println(throwable.getMessage()));
-    }
-
-
-    @Test
-    public void testRxJava03() {
-
-        Observable.just("1235")
-                .subscribe(new Subscriber<String>() {
-                    @Override
-                    public void onCompleted() {
-                        System.out.println("执行完毕 onCompleted");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        System.out.println("执行错误 onError");
-                    }
-
-                    @Override
-                    public void onNext(String s) {
-                        System.out.println("执行中     onNext");
-                    }
-                });
-
-    }
-
-
-    static class A {
-        public A(String name) {
-            this.name = name;
-        }
-
-        public String name;
     }
 
 
